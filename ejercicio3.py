c435a75dc49714dbe2d7e79792aa4a38e2b6a96c
vitoprogram <victorkabi08@gmail.com>
2025-09-19 09:08:46 +0200

    Add files via upload

---
+++ ejercicio3.py
@@ -0,0 +1,4 @@
+nombre = input("¿Cuál es tu nombre? ")
+edad = input("¿Cuántos años tienes? ")
+
+print(f"Hola {nombre}, tienes {edad} años. ¡Encantado de conocerte!")
---
+++ ejercicio4.py
@@ -0,0 +1,15 @@
+print("Dame el primer número")
+n1 = int(input())
+
+print("Dame el segundo número")
+n2 = int(input())
+
+print("Dame el tercer número")
+n3 = int(input())
+
+print("Dame el cuarto número")
+n4 = int(input())
+
+media = (n1 + n2 + n3 + n4) / 4
+
+print(f"La media es {media:.3f}")
